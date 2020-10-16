0d4e7a7cdd30fa9888c3a64b83bf12570a680a2d
Clément Pagnier <clempagnier@yahoo.fr>
2020-10-16 15:17:31 +0200

    aaaaaaaaaa

---
+++ ShmupRush/Assets/KLD/KLD_Scripts/KLD_Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_Parallax : MonoBehaviour
+{
+    [SerializeField]
+    float parallaxCoeff = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * Time.deltaTime * 2f * parallaxCoeff;
+    }
+}
